3da40826c0dd5c336cbc3e4bf380c4f9fe00d39f
chaudhary1337 <tanishqchaudhary511@gmail.com>
2021-08-19 02:53:56 -0400

    Upd: A1Q3 COMPLETE! ez gg

--- 1. System Calls/3.c
+++ 1. System Calls/3.c
@@ -31,86 +31,42 @@ int main(int argc, char *argv[])
     char *output_file_names[] = {"Assignment", output_file1_name, output_file2_name};
     // done
 
-    // loop through all of the names
-    for (int i = 0; i < 3; i++)
+    char *owner[] = {"User", "Group", "Others"};
+    char *ops[] = {"read", "write", "execute"};
+    int flags[] = {S_IRUSR, S_IWUSR, S_IXUSR, S_IRGRP, S_IWGRP, S_IXGRP, S_IROTH, S_IWOTH, S_IXOTH};
+    for (int file = 0; file < 3; file++)
     {
         // gets the satus of all the files
         struct stat fileStat;
-        if (stat(output_file_names[i], &fileStat) < 0)
+        if (stat(output_file_names[file], &fileStat) < 0)
             return 1;
 
-        // only prints this once
-        if (!i)
+        // only when file is 0, that is, the directory
+        if (!file)
         {
-            char msg[128] = "Directory is created: ";
-            strcat(msg, (S_ISDIR(fileStat.st_mode)) ? "Yes" : "No");
-            strcat(msg, "\n\n");
-            syscall(SYS_write, STDOUT_FILENO, msg, 128);
+            char dirmsg[128] = "Directory is created: ";
+            strcat(dirmsg, (S_ISDIR(fileStat.st_mode)) ? "Yes" : "No");
+            strcat(dirmsg, "\n\n");
+            syscall(SYS_write, STDOUT_FILENO, dirmsg, 128);
         }
 
-        // printf("User has read permission on %s: %s\n", output_file_names[i], (fileStat.st_mode & S_IRUSR) ? "Yes" : "No");
-        // printf("User has write permission on %s: %s\n", output_file_names[i], (fileStat.st_mode & S_IWUSR) ? "Yes" : "No");
-        // printf("User has execute permission on %s: %s\n\n", output_file_names[i], (fileStat.st_mode & S_IXUSR) ? "Yes" : "No");
-        // printf("Group has read permission on %s: %s\n", output_file_names[i], (fileStat.st_mode & S_IRGRP) ? "Yes" : "No");
-        // printf("Group has write permission on %s: %s\n", output_file_names[i], (fileStat.st_mode & S_IWGRP) ? "Yes" : "No");
-        // printf("Group has execute permission on %s: %s\n\n", output_file_names[i], (fileStat.st_mode & S_IXGRP) ? "Yes" : "No");
-        // printf("Others has read permission on %s: %s\n", output_file_names[i], (fileStat.st_mode & S_IROTH) ? "Yes" : "No");
-        // printf("Others has write permission on %s: %s\n", output_file_names[i], (fileStat.st_mode & S_IWOTH) ? "Yes" : "No");
-        // printf("Others has execute permission on %s: %s\n\n", output_file_names[i], (fileStat.st_mode & S_IXOTH) ? "Yes" : "No");
-
-        char msg1[128] = "User has read permission on ";
-        strcat(msg1, output_file_names[i]);
-        strcat(msg1, (fileStat.st_mode & S_IRUSR) ? ": Yes" : ": No");
-        strcat(msg1, "\n");
-        syscall(SYS_write, STDOUT_FILENO, msg1, 128);
-
-        char msg2[128] = "User has write permission on ";
-        strcat(msg2, output_file_names[i]);
-        strcat(msg2, (fileStat.st_mode & S_IWUSR) ? ": Yes" : ": No");
-        strcat(msg2, "\n");
-        syscall(SYS_write, STDOUT_FILENO, msg2, 128);
-
-        char msg3[128] = "User has execute permission on ";
-        strcat(msg3, output_file_names[i]);
-        strcat(msg3, (fileStat.st_mode & S_IXUSR) ? ": Yes" : ": No");
-        strcat(msg3, "\n\n");
-        syscall(SYS_write, STDOUT_FILENO, msg3, 128);
-
-        char msg4[128] = "Group has read permission on ";
-        strcat(msg4, output_file_names[i]);
-        strcat(msg4, (fileStat.st_mode & S_IRGRP) ? ": Yes" : ": No");
-        strcat(msg4, "\n");
-        syscall(SYS_write, STDOUT_FILENO, msg4, 128);
-
-        char msg5[128] = "Group has write permission on ";
-        strcat(msg5, output_file_names[i]);
-        strcat(msg5, (fileStat.st_mode & S_IWGRP) ? ": Yes" : ": No");
-        strcat(msg5, "\n");
-        syscall(SYS_write, STDOUT_FILENO, msg5, 128);
-
-        char msg6[128] = "Group has execute permission on ";
-        strcat(msg6, output_file_names[i]);
-        strcat(msg6, (fileStat.st_mode & S_IXGRP) ? ": Yes" : ": No");
-        strcat(msg6, "\n\n");
-        syscall(SYS_write, STDOUT_FILENO, msg6, 128);
-
-        char msg7[128] = "Others has read permission on ";
-        strcat(msg7, output_file_names[i]);
-        strcat(msg7, (fileStat.st_mode & S_IROTH) ? ": Yes" : ": No");
-        strcat(msg7, "\n");
-        syscall(SYS_write, STDOUT_FILENO, msg7, 128);
-
-        char msg8[128] = "Others has write permission on ";
-        strcat(msg8, output_file_names[i]);
-        strcat(msg8, (fileStat.st_mode & S_IWOTH) ? ": Yes" : ": No");
-        strcat(msg8, "\n");
-        syscall(SYS_write, STDOUT_FILENO, msg8, 128);
-
-        char msg9[128] = "Others has execute permission on ";
-        strcat(msg9, output_file_names[i]);
-        strcat(msg9, (fileStat.st_mode & S_IXOTH) ? ": Yes" : ": No");
-        strcat(msg9, "\n\n");
-        syscall(SYS_write, STDOUT_FILENO, msg9, 128);
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                char msg[128] = "";
+                strcat(msg, owner[i]);
+                strcat(msg, " has ");
+                strcat(msg, ops[j]);
+                strcat(msg, " permission on ");
+                strcat(msg, output_file_names[file]);
+                strcat(msg, ": ");
+                strcat(msg, (fileStat.st_mode & flags[3 * i + j]) ? "Yes" : "No");
+                strcat(msg, "\n");
+                syscall(SYS_write, STDOUT_FILENO, msg, 128);
+            }
+            syscall(SYS_write, STDOUT_FILENO, "\n", 1);
+        }
     }
 
     return 0;
--- 1. System Calls/test.c
+++ 1. System Calls/test.c
@@ -1,12 +1,29 @@
 #include <sys/syscall.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <stdio.h>
 
 int main()
 {
     char msg[] = "wow very nice";
     strcat(msg, " hihihi");
     syscall(SYS_write, STDOUT_FILENO, msg, sizeof(msg));
+
+    char owner[] = {"User", "Group", "Others"};
+    char ops[] = {"read", "write", "execute"};
+    int flags[] = {S_IRUSR, S_IWUSR, S_IXUSR, S_IRGRP, S_IWGRP, S_IXGRP, S_IROTH, S_IWOTH, S_IXOTH};
+    for (int i = 0; i < 3; i++)
+    {
+        char msg[128] = owner[i];
+        for (int j = 0; j < 3; j++)
+        {
+            strcat(msg, " has ");
+            strcat(msg, ops[j]);
+            strcat(msg, " permission on {}:");
+            strcat(msg, flags[3 * i + j]);
+        }
+    }
 
     return 0;
 }
