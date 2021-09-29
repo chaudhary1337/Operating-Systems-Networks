ba211a97274fcf0d07c887bf71508c810b8f8986
chaudhary1337 <tanishqchaudhary511@gmail.com>
2021-09-29 01:22:05 -0400

    [A3] pipe workds (i think)

--- 2. AntiqSH/src/pipe.c
+++ 2. AntiqSH/src/pipe.c
@@ -4,56 +4,38 @@
 
 void handle_pipe(char *segments[MAX_INPUT_LENGTH], int i)
 {
-    int old_fds[2];
-    int new_fds[2];
+    int new_pfd[2];
+    int old_pfd[2] = {STDIN_FILENO, STDOUT_FILENO};
+    int save_out = dup(STDOUT_FILENO);
+    int save_in = dup(STDOUT_FILENO);
 
     // handle each segment as if it were a command
     for (int j = 0; j < i; j++)
     {
-        if (j + 1 < i)
+        int pipe_return = pipe(new_pfd);
+        if (pipe_return < 0)
         {
-            if (pipe(new_fds) < 0)
-            {
-                printf("pipe error. call mario!\n");
-                return;
-            }
+            printf("pipe error. call mario!\n");
+            return;
         }
         pid_t pid = fork();
         if (pid < 0)
         {
-            printf("fork error in pipe. sOmBaDy ToUch mY SphAgEt");
+            printf("fork error in pipe. sOmBaDy ToUch mY SphAgEti");
             return;
         }
         else if (pid > 0) // parent
         {
-            // wait(NULL);
-            if (j > 0)
-            {
-                close(old_fds[0]);
-                close(old_fds[1]);
-            }
-            if (j + 1 < i)
-            {
-                // replacing old with new
-                // if next command present
-                old_fds[0] = new_fds[0];
-                old_fds[1] = new_fds[1];
-            }
+            wait(NULL);
+            close(new_pfd[1]);       // closing the write end of pipe
+            old_pfd[0] = new_pfd[0]; // saving the old pipe
         }
         else //child
         {
-            if (j > 0)
-            {
-                dup2(old_fds[0], 0);
-                close(old_fds[0]);
-                close(old_fds[1]);
-            }
-            if (j + 1 < i)
-            {
-                close(new_fds[0]);
-                dup2(new_fds[1], 1);
-                close(new_fds[1]);
-            }
+            dup2(old_pfd[0], STDIN_FILENO); // reading from the pipe of last command.
+            if (j + 1 != i)                 // if not the end, stdout to curr pipe
+                dup2(new_pfd[1], STDOUT_FILENO);
+            close(new_pfd[0]);
 
             char *args[MAX_ARGS];
             get_args(segments[j], args);
@@ -66,5 +48,7 @@ void handle_pipe(char *segments[MAX_INPUT_LENGTH], int i)
         }
     }
 
+    dup2(save_in, STDIN_FILENO);
+    dup2(save_out, STDOUT_FILENO);
     return;
 }
