0f7828fd451072b37217c03c1f2c7e79ccfa41bb
chaudhary1337 <tanishqchaudhary511@gmail.com>
2021-10-02 06:30:44 -0400

    [A3] Fix some redirect issues

--- 2. AntiqSH/src/redirect.c
+++ 2. AntiqSH/src/redirect.c
@@ -2,7 +2,7 @@
 #include "misc.h"
 #include "my_command.h"
 
-void handle_input_redirect(char *args[MAX_ARGS])
+int handle_input_redirect(char *args[MAX_ARGS])
 {
     int i = 0;
     while (args[i])
@@ -12,12 +12,12 @@ void handle_input_redirect(char *args[MAX_ARGS])
             if (i == 0)
             {
                 printf("who do i feed this input to, huh?\n");
-                return;
+                return 1;
             }
             if (!args[i + 1])
             {
                 printf("where input??\n");
-                return;
+                return 1;
             }
 
             // open the file
@@ -25,7 +25,7 @@ void handle_input_redirect(char *args[MAX_ARGS])
             if (fd_in < 0)
             {
                 printf("some error in opening file :hmmm:\n");
-                return;
+                return 1;
             }
 
             dup2(fd_in, STDIN_FILENO); // send the data to file
@@ -33,17 +33,23 @@ void handle_input_redirect(char *args[MAX_ARGS])
 
             // shift the args by 2
             while (args[i])
+            {
                 args[i] = args[i + 2];
+                i++;
+            }
 
             // if found one redirect, yeeettt
+            int x = 0;
+            while (args[x])
+                printf("args in redir: %s\n", args[x++]);
             break;
         }
         i++;
     }
-    return;
+    return 0;
 }
 
-void handle_output_redirect(char *args[MAX_ARGS])
+int handle_output_redirect(char *args[MAX_ARGS])
 {
     int i = 0;
     while (args[i])
@@ -54,12 +60,12 @@ void handle_output_redirect(char *args[MAX_ARGS])
             if (i == 0)
             {
                 printf("where do I get this output from, huh?\n");
-                return;
+                return 1;
             }
             if (!args[i + 1])
             {
                 printf("where output??\n");
-                return;
+                return 1;
             }
 
             // open the file
@@ -72,34 +78,43 @@ void handle_output_redirect(char *args[MAX_ARGS])
             if (fd_out < 0)
             {
                 printf("some error in opening file :hmmm:\n");
-                return;
+                return 1;
             }
 
             dup2(fd_out, STDOUT_FILENO); // send the data to file
             close(fd_out);
 
-            // shift the args by 2
-            while (args[i])
-                args[i] = args[i + 2];
+            // // shift the args by 2
+            // while (args[i])
+            // {
+            //     args[i] = args[i + 2];
+            //     i++;
+            // }
 
             // if found one redirect, yeeettt
             break;
         }
         i++;
     }
-    return;
+    return 0;
 }
 
 void handle_redirect(char *segment)
 {
     char *args[MAX_ARGS];
     get_args(segment, args);
-    handle_input_redirect(args);
-    handle_output_redirect(args);
-
-    // int x = 0;
-    // while (args[x])
-    //     printf("args in redir: %s\n", args[x++]);
+    int input_redirect_return = handle_input_redirect(args);
+    if (input_redirect_return)
+    {
+        exit(0);
+        return;
+    }
+    int output_redirect_return = handle_output_redirect(args);
+    if (output_redirect_return)
+    {
+        exit(0);
+        return;
+    }
 
     int exec_return = execvp(args[0], args);
     if (exec_return < 0) // if invalid command, print its invalid and exit
