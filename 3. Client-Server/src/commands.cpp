17317e60e20664e33077cb0fe605c7906c7d461a
chaudhary1337 <tanishqchaudhary511@gmail.com>
2021-11-24 04:59:06 -0500

    [A5] Completed basic server. May need to stress test concurrency part.

--- 3. Client-Server/src/commands.cpp
+++ 3. Client-Server/src/commands.cpp
@@ -20,6 +20,7 @@ string handle_insert(char *args[MAX_ARGS])
 
     if (dict[key].length())
         return "Key already exists\n";
+
     pthread_mutex_lock(&my_dict_mutex[key]);
     dict[key] = value;
     pthread_mutex_unlock(&my_dict_mutex[key]);
@@ -54,8 +55,58 @@ string handle_delete(char *args[MAX_ARGS])
     if (!dict[key].length())
         return "No such key exists\n";
 
+    pthread_mutex_lock(&my_dict_mutex[key]);
     dict[key] = "";
+    pthread_mutex_unlock(&my_dict_mutex[key]);
     return "Deletion successful\n";
+}
+
+string handle_update(char *args[MAX_ARGS])
+{
+    if (!(args[0] && args[1] && args[2]))
+        return "Invalid #args\n";
+
+    int key = atoi(args[1]);
+    if (!(0 <= key && key <= 100))
+        return "Invalid key\n";
+    string value = args[2];
+
+    if (!dict[key].length())
+        return "Key dones not exist\n";
+
+    pthread_mutex_lock(&my_dict_mutex[key]);
+    dict[key] = value;
+    pthread_mutex_unlock(&my_dict_mutex[key]);
+
+    return "Updated value: " + value + "\n";
+}
+
+string handle_concat(char *args[MAX_ARGS])
+{
+    if (!(args[0] && args[1] && args[2]))
+        return "Invalid #args\n";
+
+    int key1 = atoi(args[1]);
+    if (!(0 <= key1 && key1 <= 100))
+        return "Invalid key1\n";
+    int key2 = atoi(args[2]);
+    if (!(0 <= key2 && key2 <= 100))
+        return "Invalid key2\n";
+
+    string temp1 = dict[key1];
+    string temp2 = dict[key2];
+
+    if (!(temp1.length() and temp2.length()))
+        return "Concat failed as at least one of the keys does not exist\n";
+
+    pthread_mutex_lock(&my_dict_mutex[key1]);
+    pthread_mutex_lock(&my_dict_mutex[key2]);
+    dict[key1] = temp1 + temp2;
+    dict[key2] = temp2 + temp1;
+    pthread_mutex_unlock(&my_dict_mutex[key2]);
+    pthread_mutex_unlock(&my_dict_mutex[key1]);
+
+    return "Concat value: " + dict[key2] + "\n";
 }
 
 string handle_commands(char *user_input)
@@ -79,12 +130,12 @@ string handle_commands(char *user_input)
         response = handle_insert(args);
     else if (!strncmp(args[0], "fetch", 6))
         response = handle_fetch(args);
-    // else if (!strncmp(args[0], "delete", 6))
-    //     response = handle_delete(args);
-    // else if (!strncmp(args[0], "update", 6))
-    //     response = handle_update(args);
-    // else if (!strncmp(args[0], "concat", 6))
-    //     response = handle_concat(args);
+    else if (!strncmp(args[0], "delete", 6))
+        response = handle_delete(args);
+    else if (!strncmp(args[0], "update", 6))
+        response = handle_update(args);
+    else if (!strncmp(args[0], "concat", 6))
+        response = handle_concat(args);
 
     return response;
 }
--- 3. Client-Server/test.sh
+++ 3. Client-Server/test.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-echo "insert 5 wow5" | nc kali 1337
-echo "insert 5 wow5 second time" | nc kali 1337
-echo "insert 2 wow2" | nc kali 1337
-echo "insert 8 wow8" | nc kali 1337
-echo "insert 8 wow8again" | nc kali 1337
-echo "insert 8 wow8again&again" | nc kali 1337
-echo "insert 8 fux" | nc kali 1337
-echo "fetch 2" | nc kali 1337
-echo "fetch 5" | nc kali 1337
+echo "insert 1 hello" | nc kali 1337
+echo "insert 1 hello" | nc kali 1337
+echo "insert 2 yes" | nc kali 1337
+echo "insert 3 no" | nc kali 1337
+echo "concat 1 2" | nc kali 1337
+echo "concat 1 3" | nc kali 1337
+echo "delete 3" | nc kali 1337
+echo "delete 4" | nc kali 1337
+echo "concat 1 4" | nc kali 1337
+echo "update 1 final" | nc kali 1337
+echo "concat 1 2" | nc kali 1337
+
